a970162226d4ea58080e095e912ef52115e357f2
Chenggong Wang <c.wang@princeton.edu>
2023-06-20 11:23:20 -0400

    post process hourly data: bias, mse, and rmse

---
+++ GCM_results/monthavg_erorr_file.py
@@ -0,0 +1,122 @@
+import xarray as xr  
+import time 
+import argparse, sys
+import os 
+import time
+import psutil
+import gc
+ori_var = [
+ 'lwdn_sfc',
+ # 'lwup_sfc',
+ 'swdn_sfc',
+ 'swup_sfc',
+ 'olr',
+ 'swdn_toa',
+ 'swup_toa',
+ 'tdt_lw',
+ 'tdt_sw',
+ 'tdt_lw_clr',
+ 'tdt_sw_clr',
+ 'lwdn_sfc_clr',
+ 'swdn_sfc_clr',
+ 'swup_sfc_clr',
+ 'olr_clr',
+ 'swup_toa_clr',]
+def build_var_list(ds, orivar):
+    err_var = []
+    for _ in orivar:
+        if _ in list(ds.variables) : 
+            # check nn var
+            if f'nn_{_}' in list(ds.variables) : 
+                err_var.append(_) 
+            else:
+                print(f'nn_{_} do not exist in data file. Please check!')
+        else:
+            print(f'{_} do not exist in data file. Please check!')
+    return err_var
+
+if __name__ == '__main__':  
+    ##################################################### 
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--filename", help="filepattern, hourly, daily file name before .nc") 
+    args=parser.parse_args()
+    filename = args.filename 
+    # file_path = '/scratch/gpfs/cw55/AM4/work/CTL2000_sst0.5Kperyear_nn_stellarcpu_intelmpi_22_768PE_base/'
+    # filename = file_path+f'POSTP/19810101.atmos_8xdaily.nc'
+    
+    zero_time = time.time()
+    file_stats = os.stat(filename)
+    print(f"Converting file: {filename} | Raw file size: {file_stats.st_size / (1024**3):5.1f} GB ")
+    os.system(f'cp --parents {filename} /dev/shm/')
+    print("load data into /dev/shm")
+    with xr.open_dataset(f"/dev/shm/{filename}") as ds: 
+        var_list=build_var_list(ds,ori_var)
+        lwup_sfc = ds['nn_lwup_sfc'].load()
+        ds_new = lwup_sfc.groupby('time.month').mean('time').to_dataset()
+    for svar in var_list:
+        # warp operations in with statement to manage the memory better
+        with xr.open_dataset(f"/dev/shm/{filename}") as ds: 
+            sta_time = time.time()
+            print(f'Read {svar:12s} ...', end='')
+            ## Getting % usage of virtual_memory ( 3rd field)
+            #print('RAM memory % used:', psutil.virtual_memory()[2],end = '')
+            ## Getting usage of virtual_memory in GB ( 4th field)
+            #print('RAM Used (GB):', psutil.virtual_memory()[3]/1000000000)
+            da = ds[svar].load()
+            nnda = ds[f'nn_{svar}'].load() 
+            ds_new[svar]         = da.groupby('time.month').mean('time')
+            ds_new[f'nn_{svar}'] = nnda.groupby('time.month').mean('time')
+            ## Getting % usage of virtual_memory ( 3rd field)
+            #print('RAM memory % used:', psutil.virtual_memory()[2],end = '')
+            ## Getting usage of virtual_memory in GB ( 4th field)
+            #print('RAM Used (GB):', psutil.virtual_memory()[3]/1000000000)
+            #gc.collect()
+
+            err = nnda - da
+            del da, nnda
+            ds_new[f'bias_{svar}'] = err.groupby('time.month').mean('time')
+            ds_new[f'mse_{svar}']  = abs(err).groupby('time.month').mean('time')
+            ds_new[f'rmse_{svar}'] = ((err**2).groupby('time.month').mean('time'))**0.5
+
+            err_adj = xr.where(lwup_sfc<0,0,err)
+            ds_new[f'adj_bias_{svar}'] = err_adj.groupby('time.month').mean('time')
+            ds_new[f'adj_mse_{svar}']  = abs(err_adj).groupby('time.month').mean('time')
+            ds_new[f'adj_rmse_{svar}'] = ((err_adj**2).groupby('time.month').mean('time'))**0.5
+            print(f' done. Use time: {time.time() - sta_time: 3.0f}s')
+            del err, err_adj
+    # add time axis
+    ds_new = ds_new.rename({'month':'time'}) 
+    new_time = xr.concat([_[1][0] for _ in ds.time.groupby('time.month')],dim='time')
+    ds_new['time'] = new_time
+    ds_new.to_netcdf(f"{filename}.monavg_error.nc")
+    file_stats_new = os.stat(f"{filename}.monavg_error.nc")
+    used_time = time.time() - zero_time  
+    print(f'    | New Size: {file_stats_new.st_size / (1024**3):5.1f} GB | timer: {used_time:4.0f}s')
+    os.system(f'rm -f  /dev/shm/{filename}')
+    
+# import xarray as xr  
+# import time 
+# import argparse, sys
+# import os 
+
+# if __name__ == '__main__':  
+#     sta_time = time.time()
+#     ##################################################### 
+#     parser=argparse.ArgumentParser()
+#     parser.add_argument("--filename", help="filepattern, hourly, daily file name before .nc") 
+#     args=parser.parse_args()
+#     filename = args.filename 
+
+#     file_stats = os.stat(filename)
+#     print(f"Converting file: {filename} | Raw file size: {file_stats.st_size / (1024**3):5.1f} GB ")
+#     with xr.open_dataset(filename) as ds:
+#         # ds = ds.load() # slower?
+#         ds_new = ds.groupby('time.month').mean('time')
+#         # add time axis
+#         ds_new = ds_new.rename({'month':'time'}) 
+#         new_time = xr.concat([_[1][0] for _ in ds.time.groupby('time.month')],dim='time')
+#         ds_new['time'] = new_time
+#         ds_new.to_netcdf(f"{filename}.monavg.nc")
+#     used_time = time.time() - sta_time  
+#     file_stats_new = os.stat(f"{filename}.monavg.nc")
+#     print(f'    | New Size: {file_stats_new.st_size / (1024**3):5.1f} GB | timer: {used_time:4.0f}s')
---
+++ GCM_results/run_monavg_ERORR.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH --nodes=1 # node count
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1     # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem=300G
+#SBATCH -t 24:00:00
+# Sends mail when process begins, and when it ends. 
+# Make sure you define your email
+#SBATCH --mail-type=all
+#SBATCH --mail-user=cw55@princeton.edu
+
+###################################################################
+#Script Name : run_nn_ratio.sh
+#Description : ~
+#Args        :
+#Author      : Chenggong Wang
+#Email       : c.wang@princeton.edu
+###################################################################
+
+set -e #end with any error
+#set -x #expands variables and prints a little + sign before the line
+# load modules or conda environments here
+source /usr/share/Modules/init/bash
+
+module purge
+module load anaconda3/2023.3
+
+source /usr/licensed/anaconda3/2023.3/etc/profile.d/conda.sh
+conda activate cg310
+which python
+echo "Date:"
+echo $(date +%y%m%d%H%M)
+ROOT=$PWD
+echo "Current dir:"
+echo $ROOT
+for f in $ROOT/*8xdaily.nc
+do 
+    echo "$f"
+    new_f="$f.monavg_error.nc"
+    if test -f "$new_f"; then
+        echo "$new_f exists."
+    else
+        # echo "$new_f not exists."
+        # cdo monavg "$f" "$new_f"
+        python -u monthavg_erorr_file.py --filename "$f"
+    fi
+
+done
+echo $(date +%y%m%d%H%M)
+echo END
